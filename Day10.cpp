d8b078077aaa475e80454780f04e8e8a6e2f5d73
SouravPareek <148560393+SouravPareek@users.noreply.github.com>
2025-04-15 00:02:20 +0530

    Create Day10.cpp

---
+++ Day10.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int maxSubarraySum(vector<int> &arr) {
+        int n=arr.size();
+        int mx=arr[0];
+        int res=arr[0];
+        for(int i=1;i<n;i++){
+            mx=max(mx+arr[i],arr[i]);
+            res=max(res,mx);
+        }
+        return res;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // To discard any leftover newline characters
+    while (t--)   // while testcases exist
+    {
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        cout << ob.maxSubarraySum(arr) << endl << "~" << endl;
+    }
+}
+// } Driver Code Ends
